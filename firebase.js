6670e062c7f73f3872e834381b878d60bcc7932f
Dana1127 <danag20027@gmail.com>
2022-10-31 23:49:40 -0500

    Inicializar archivos base

---
+++ firebase.js
@@ -0,0 +1,17 @@
+        // Import the functions you need from the SDKs you need
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+      
+        // Your web app's Firebase configuration
+        const firebaseConfig = {
+          apiKey: "AIzaSyA0zk9UtcESSCmXhPPUbqb_VqRNaqQfdw8",
+          authDomain: "fir-prog-web.firebaseapp.com",
+          projectId: "fir-prog-web",
+          storageBucket: "fir-prog-web.appspot.com",
+          messagingSenderId: "886358910534",
+          appId: "1:886358910534:web:03e36396fe22f4452d8c47"
+        };
+      
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);

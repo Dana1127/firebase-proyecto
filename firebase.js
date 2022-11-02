82909230698322dcac4931755684e785f45c237c
Dana1127 <danag20027@gmail.com>
2022-11-02 12:00:57 -0500

    Actualizar firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
         // Import the functions you need from the SDKs you need
         import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+        import { getFirestore, collection, addDoc} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js"
         // TODO: Add SDKs for Firebase products that you want to use
         // https://firebase.google.com/docs/web/setup#available-libraries
       
@@ -15,3 +16,19 @@
       
         // Initialize Firebase
         const app = initializeApp(firebaseConfig);
+
+        const db = getFirestore(app);
+
+        async function addTask(){
+          try {
+            const docRef = await addDoc(collection(db, "users"), {
+              first: "Ada",
+              last: "Lovelace",
+              born: 1815
+            });
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+          
+        }
